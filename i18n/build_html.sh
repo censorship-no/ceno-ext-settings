ab7318883812bf29623697ce31dea92648f26bf9
jnny <jenny@sudomesh.org>
2021-02-03 13:10:22 -0800

    fixing html pagebuilder

--- i18n/build_html.sh
+++ i18n/build_html.sh
@@ -7,19 +7,19 @@ cd "$(dirname "$0")"
 mkdir -p html
 
 # Build english html files
-po2html -i _locales/en -t ../ -o html/en
+po2html -i po/en -t ../ -o html/en
 
 # Build spanish html files
-po2html -i _locales/es_ES -t ../ -o html/es_ES
+po2html -i po/es_ES -t ../ -o html/es_ES
 
 # Build farsi html files
-po2html -i _locales/fa -t ../ -o html/fa
+po2html -i po/fa -t ../ -o html/fa
 
 # Build french html files
-po2html -i _locales/fr -t ../ -o html/fr
+po2html -i po/fr -t ../ -o html/fr
 
 # Build russian html files
-po2html -i _locales/ru -t ../ -o html/ru
+po2html -i po/ru -t ../ -o html/ru
 
 # Build chinese html files
-po2html -i _locales/zh_CN -t ../ -o html/zh_CN
+po2html -i po/zh_CN -t ../ -o html/zh_CN
