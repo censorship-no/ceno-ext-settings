5f9d96c82dec6903678acba49dcf8d657825770b
paidforby <20746091+paidforby@users.noreply.github.com>
2025-03-03 12:08:05 -0500

    Remove unused files

--- button.js
+++
@@ -1,5 +0,0 @@
-var openSettingsTab = function() {
-  var url = browser.extension.getURL("settings.html");
-  browser.tabs.create({url: url});
-}
-browser.browserAction.onClicked.addListener(openSettingsTab);
